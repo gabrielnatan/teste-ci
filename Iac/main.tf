4192a9c1dff7f5d2800e5d8bc2ee32d78e7369a4
Gabriel Natan <gabriel--natan@live.com>
2024-12-23 15:22:27 -0300

    init project

---
+++ Iac/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.82.2"
+    }
+  }
+
+  backend "s3" {
+    bucket = "s3-bucket-ap-southeast-2-maneiro"
+    key    = "state/terraform.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
+provider "aws" {
+  alias   = "us-east-1"
+  region  = "us-east-1"
+  profile = "default"
+}
+
+provider "aws" {
+  alias   = "ap-southeast-2"
+  region  = "ap-southeast-2"
+  profile = "default"
+}
+
+resource "aws_instance" "aws-ec2-maneira" {
+  provider = aws.us-east-1
+
+  instance_type = "t2.micro"
+  ami           = "ami-0e2c8caa4b6378d8c"
+
+  tags = {
+    Iac  = "True"
+    Name = "Maneira 1"
+  }
+}
+
+
---
+++ Iac/s3.tf
@@ -0,0 +1,39 @@
+# Recurso S3 Bucket em ap-southeast-2
+resource "aws_s3_bucket" "s3" {
+  provider = aws.ap-southeast-2
+
+  bucket        = "s3-bucket-ap-southeast-2-maneiro"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = "S3 Bucket"
+    Iac  = "True"
+  }
+}
+
+# resource "aws_s3_bucket" "s3" {
+#   bucket        = "s3-bucket"
+#   force_destroy = true
+
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+
+#   tags = {
+#     Name = "S3 Bucket"
+#     Iac  = "True"
+#   }
+# }
+
+
+# resource "aws_s3_bucket_versioning" "versioning_s3" {
+#   bucket = aws_s3_bucket.s3.bucket
+
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
